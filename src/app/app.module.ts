4519d9124020b70dc7882b00d6a147db5fcf482d
cheong.kawin@gmail.com <cheong.kawin@gmail.com>
2019-03-20 17:17:42 +0800

    Added FAB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';;
 
+/*Angular Material*/
+import {MatMenuModule} from '@angular/material/menu';
 
 
 @NgModule({
@@ -14,7 +17,9 @@ import { HomePageComponent } from './home-page/home-page.component';;
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
