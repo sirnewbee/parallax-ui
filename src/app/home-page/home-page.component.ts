e123cb46f3bf747fbb317f98c44e52586409bf81
cheong.kawin@gmail.com <cheong.kawin@gmail.com>
2019-03-21 15:47:27 +0800

    Redeploy

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -23,13 +23,14 @@ export class HomePageComponent implements OnInit {
       this.bounding.top >= 0 && this.bounding.left >= 0 && this.bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && this.bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
       console.log('In the viewport!');
       document.getElementById("fab").style.visibility = "hidden";
+      this.menu = false;
     } else {
       console.log('Not in the viewport... whomp whomp');
       document.getElementById("fab").style.visibility = "visible";
     }
   }
 
-  toggleMenu(){
-    this.menu = !this.menu;
+  toggleMenu(data){
+    this.menu = data;
   }
 }
